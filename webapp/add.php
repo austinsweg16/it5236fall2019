309eac97695b8e81ab381d68d014c0aa4094b87b
austinsweg16 <46796789+austinsweg16@users.noreply.github.com>
2019-10-29 10:56:55 -0400

    Update add.php

--- webapp/add.php
+++ webapp/add.php
@@ -1,24 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-$dbconnecterror = FALSE;
-$dbh = NULL;
-
-require_once 'credentials.php';
-
-try{
-	
-	$conn_string = "mysql:host=".$dbserver.";dbname=".$db;
-	
-	$dbh= new PDO($conn_string, $dbusername, $dbpassword);
-	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	
-}catch(Exception $e){
-	$dbconnecterror = TRUE;
-}
-
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 	
 	if (array_key_exists('fin', $_POST)) {
@@ -33,24 +13,31 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 		$finBy = $_POST['finBy'];
 	}
 
-	if (!$dbconnecterror) {
-		try {
-			$sql = "INSERT INTO doList (complete, listItem, finishDate) VALUES (:complete, :listItem, :finishDate)";
-			$stmt = $dbh->prepare($sql);			
-			$stmt->bindParam(":complete", $complete);
-			$stmt->bindParam(":listItem", $_POST['listItem']);
-			$stmt->bindParam(":finishDate", $finBy);
-			$response = $stmt->execute();	
+	$url = 'http://3.19.209.253/api/task.php';
+	
+	$data = json_encode([
+		'taskName' => $_POST['listItem'],
+		'taskDate' => $finBy,
+		'completed' => $complete
+	]);
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, [
+		'Content-Type: application/json',
+		'Content-Length: ' . strlen($data)
+	]);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$response = curl_exec($ch);
+	$http_status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	curl_close($ch);
 			
-			header("Location: index.php");
-			
-		} catch (PDOException $e) {
-			header("Location: index.php?error=add");
-		}	
+	if ($http_status_code == 201) {
+		header("Location: index.php");
 	} else {
 		header("Location: index.php?error=add");
 	}
 }
-
-
-?>
+ 

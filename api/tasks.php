07e1b3fb7523579a5677825c7bf603d194ff814d
austinsweg16 <46796789+austinsweg16@users.noreply.github.com>
2019-10-29 10:59:16 -0400

    Update tasks.php

--- api/tasks.php
+++ api/tasks.php
@@ -1,9 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 // Declare the credentials to the database
 $dbh = NULL;
     
@@ -27,8 +22,28 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
 		$stmt = $dbh->prepare($sql);
 		$stmt->execute();
 		$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		
+		// listID  -> listID
+		// listItem -> taskName
+		// finishDate -> taskDate
+		// complete -> completed
+		
+		$final = [];
+		foreach($result as $task) {
+			// Copy the task info into the new, proper keys
+			$task['taskName'] = $task['listItem'];
+			$task['taskDate'] = $task['finishDate'];
+			$task['completed'] = $task['complete'];
+			$task['completed'] = $task['completed'] == "1" ? true : false;
+
+			// Delete the old keys
+			unset($task['listItem'], $task['finishDate'], $task['complete']);
+			
+			$final[] = $task;
+		}
+		
 		http_response_code(200);
-		echo json_encode($result);
+		echo json_encode($final);
 		exit();
 	} catch (PDOException $e) {
 		http_response_code(500);
